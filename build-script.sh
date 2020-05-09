0ba7e6637ed798cdcc90fcf7a4ff9a485ce671c4
John Beech <john.beech@bbc.co.uk>
2020-05-09 10:52:27 +0100

    Update build script to include licenses

--- build-script.sh
+++ build-script.sh
@@ -1,9 +1,12 @@
 rm -rf build || true
 mkdir -p build/xdfmadev
 mkdir -p build/xdfmadev/js
+mkdir -p build/xdfmadev/js/licenses
 
 git submodule init
 git submodule update
+
+cp external/DFMA-Viewer-HTML5/licenses/* build/xdfmadev/js/licenses
 
 cp external/DFMA-Viewer-HTML5/public/*.js build/xdfmadev/js
 cp external/DFMA-Viewer-HTML5/public/*.html build/xdfmadev/js
