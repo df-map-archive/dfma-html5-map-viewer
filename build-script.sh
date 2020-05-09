1aca9050bcbbaf0d21577007aae3c00a5b0d8cc0
John Beech <john.beech@bbc.co.uk>
2020-05-09 17:23:23 +0100

    #3 Add parcel to dfma-html5-map-viewer and update build pipeline See: https://github.com/df-map-archive/dfma-html5-map-viewer/issues/3

--- build-script.sh
+++ build-script.sh
@@ -1,14 +1,20 @@
 rm -rf build || true
+rm -rf dist || true
+
 mkdir -p build/xdfmadev
 mkdir -p build/xdfmadev/js
 mkdir -p build/xdfmadev/js/licenses
+mkdir -p build/xdfmadev/parcel
 
 git submodule init
 git submodule update
 
-cp external/DFMA-Viewer-HTML5/licenses/* build/xdfmadev/js/licenses
+npm run parcel
 
+cp external/DFMA-Viewer-HTML5/licenses/* build/xdfmadev/js/licenses
 cp external/DFMA-Viewer-HTML5/public/*.js build/xdfmadev/js
 cp external/DFMA-Viewer-HTML5/public/*.html build/xdfmadev/js
 cp external/DFMA-Viewer-HTML5/public/*.css build/xdfmadev/js
 cp external/DFMA-Viewer-HTML5/public/*.fdf-map build/xdfmadev/js
+
+cp dist/* build/xdfmadev/parcel
