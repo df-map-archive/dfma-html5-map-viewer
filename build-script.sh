d7ded654b230ee7597cf4ac92b7ce3fe91f0b5a0
John Beech <john.beech@bbc.co.uk>
2020-06-11 11:28:07 +0100

    Update build script and workflow

--- build-script.sh
+++ build-script.sh
@@ -2,19 +2,8 @@ rm -rf build || true
 rm -rf dist || true
 
 mkdir -p build/xdfmadev
-mkdir -p build/xdfmadev/js
-mkdir -p build/xdfmadev/js/licenses
 mkdir -p build/xdfmadev/parcel
 
-git submodule init
-git submodule update
+npm run parcel -s
 
-npm run parcel
-
-cp external/DFMA-Viewer-HTML5/licenses/* build/xdfmadev/js/licenses
-cp external/DFMA-Viewer-HTML5/public/*.js build/xdfmadev/js
-cp external/DFMA-Viewer-HTML5/public/*.html build/xdfmadev/js
-cp external/DFMA-Viewer-HTML5/public/*.css build/xdfmadev/js
-cp external/DFMA-Viewer-HTML5/public/*.fdf-map build/xdfmadev/js
-
-cp dist/* build/xdfmadev/parcel
+cp -r dist/* build/xdfmadev/parcel
