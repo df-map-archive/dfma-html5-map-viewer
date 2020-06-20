c8a5d2bbd216437ace7ec223f155fe7c270c780b
John Beech <john.beech@bbc.co.uk>
2020-06-20 21:20:06 +0100

    Add comments and note side effects to zoom-to.js

--- src/components/viewState/zoom-to.js
+++ src/components/viewState/zoom-to.js
@@ -1,11 +1,29 @@
 /**
-   * Zoom to a POI
-   * layer - layer to zoom to
-   * scale - zoom scale (zoom amount)
-   * xTile - xTile to zoom to
-   * yTile - yTile to zoom to
-   */
+ * Configure methods by injecting in shared viewState dependency
+ *
+ * @param {object} viewState
+ */
 export default function (viewState) {
+  /**
+   * Zoom to a POI
+   *
+   * Side effects:
+   * - read viewState.dfMapData for tileWidth, tileHeight, and mapData
+   * - read viewState for idx, scale,
+   *   imgWidth, originalImgWidth,
+   *   imgHeight, originalImgHeight,
+   *   canvasWidth, canvasHeight
+   * - calls console.log
+   * - assigns viewState.idx, viewState.scale, viewState.imgWidth, viewState.imgHeight
+   *   viewState.imageX, viewState.imageY
+   *
+   * @param {integer} layer the layer to zoom to
+   * @param {number} pscale the zoom scale (zoom amount)
+   * @param {integer} xTile the xTile to zoom to
+   * @param {integer} yTile the yTile to zoom to
+   *
+   * @return null manipulates viewState as a side effect
+   */
   return (layer, pscale, xTile, yTile) => {
     // find the desired layer
     let found = false
