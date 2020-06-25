594ad4b9e10b39c618c9a0cef051365bf8886a6e
John Beech <johnbeech.github@mkv25.net>
2020-06-25 22:21:41 +0100

    #20 Add build number / commit hash to map viewer so that its visible on the hosted page See: https://github.com/df-map-archive/dfma-html5-map-viewer/issues/20

---
+++ scripts/setBuildInformation.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+let buildHash
+try {
+  buildHash = fs.readFileSync(path.join(__dirname, '../build/buildhash.txt'), 'utf8')
+} catch (ex) {
+  buildHash = 'UNKNOWN'
+}
+
+const date = new Date()
+const source = `https://github.com/df-map-archive/dfma-php-private/commit/${buildHash}`
+
+const buildInfo = {
+  buildHash,
+  date,
+  source
+}
+
+fs.writeFileSync(path.join(__dirname, '../src/buildInfo.json'), JSON.stringify(buildInfo,null,2), 'utf8')
+
+console.log('[Set Build Information]', buildInfo)
