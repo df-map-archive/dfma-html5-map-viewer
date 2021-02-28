e1b8b72aa96ad336fe1cf0a8c4b4d506182ce814
John Beech <johnbeech.github@mkv25.net>
2021-02-28 22:56:42 +0000

    Refine tag and build process

--- build-script.sh
+++ build-script.sh
@@ -7,7 +7,11 @@ if [[ -z "$GITHUB_REF" ]]; then
   destinationFolder="build/xdfmadev/parcel"
 else
   RELEASE_VERSION=${GITHUB_REF#refs/*/}
-  destinationFolder="build/viewer/js/$RELEASE_VERSION"
+  if [ "$RELEASE_VERSION" == "master" ]; then
+    destinationFolder="build/viewer/js/latest/"
+  else
+    destinationFolder="build/viewer/js/$RELEASE_VERSION"
+  fi 
 fi
 
 echo "Making $destinationFolder"
